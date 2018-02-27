a6bcf81c0a1a83af1420ccad9f9ee8db644d2029
lidongze91 <lidongze91@gmail.com>
2018-02-27 11:27:49 -0800

    change url scheme

--- FashVibesAPI/urls.py
+++ FashVibesAPI/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('profiles', include('profiles_api.urls'))
+    path('profiles/', include('profiles_api.urls'))
 ]
